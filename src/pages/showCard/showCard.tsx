daeb7ccb6bb09c3d6b385b4e03fb4555fa8fd645
aminnateghi10 <aminnateghi10@gmail.com>
2023-06-25 12:24:49 +0330

    add Tts.speak for speak text

--- src/pages/showCard/showCard.tsx
+++ src/pages/showCard/showCard.tsx
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import { Button, FlatList, Modal, StyleSheet, Text, TouchableOpacity, useColorScheme, View } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import Card from "./components/card";
+import Tts from "react-native-tts";
 
 const ShowCard = ({ route, navigation }: any) => {
   const isDarkMode = useColorScheme() === "dark";
   const backgroundStyle = { backgroundColor: isDarkMode ? Colors.darker : Colors.lighter };
   const { data } = route.params;
-  console.log(data, "mm");
   const [lang, setLang] = useState(true);
+
+  let toggleLang = () => {
+    if (!lang) Tts.speak(data.english);
+    setLang(!lang);
+  };
 
   return (
     <View style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
-        <TouchableOpacity style={Styles.card} onPress={() => setLang(!lang)}>
+      <TouchableOpacity style={Styles.card} onPress={toggleLang}>
         <Text style={{ fontSize: 30, color: "rgb(0,0,0)" }}>{lang ? data.english : data.persian}</Text>
       </TouchableOpacity>
     </View>
@@ -24,8 +29,8 @@ export default ShowCard;
 const Styles = StyleSheet.create({
   card: {
     backgroundColor: "rgba(0,132,255,0.43)",
-    width: "60%",
-    height: "60%",
+    width: "80%",
+    height: "80%",
     borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
