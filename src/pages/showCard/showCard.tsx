6b82ebcf3e2c34389c5cb91aec791e0fbdbac97c
aminnateghi10 <aminnateghi10@gmail.com>
2023-06-25 13:01:11 +0330

    install icons

--- src/pages/showCard/showCard.tsx
+++ src/pages/showCard/showCard.tsx
@@ -1,5 +1,16 @@
-import React, { useState } from "react";
-import { Button, FlatList, Modal, StyleSheet, Text, TouchableOpacity, useColorScheme, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  FlatList,
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  useColorScheme,
+  View
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import Card from "./components/card";
 import Tts from "react-native-tts";
@@ -10,15 +21,22 @@ const ShowCard = ({ route, navigation }: any) => {
   const { data } = route.params;
   const [lang, setLang] = useState(true);
 
-  let toggleLang = () => {
-    if (!lang) Tts.speak(data.english);
-    setLang(!lang);
-  };
+  let sayAgain = () => Tts.speak(data.english);
+
+  useEffect(() => {
+    Tts.speak(data.english);
+  }, []);
 
   return (
     <View style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
-      <TouchableOpacity style={Styles.card} onPress={toggleLang}>
+      <TouchableOpacity style={Styles.card} onPress={() => setLang(!lang)}>
         <Text style={{ fontSize: 30, color: "rgb(0,0,0)" }}>{lang ? data.english : data.persian}</Text>
+        {
+          lang &&
+          <TouchableHighlight style={Styles.sayAgainCard} onPress={sayAgain}>
+            <Icon name="refresh" size={27} color="rgb(0,49,255)" />
+          </TouchableHighlight>
+        }
       </TouchableOpacity>
     </View>
   );
@@ -36,5 +54,13 @@ const Styles = StyleSheet.create({
     justifyContent: "center",
     elevation: 1,
     opacity: .8
+  },
+  sayAgainCard: {
+    position: "absolute",
+    bottom: 20,
+    left: "45%",
+    backgroundColor: "rgb(255,85,0)",
+    padding: 10,
+    borderRadius: 50
   }
 });
