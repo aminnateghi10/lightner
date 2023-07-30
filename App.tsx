32fa0e616022c9ede72573d6a93b966d05d9656c
Amin Nateghi <aminnateghi10@gmail.com>
2023-07-30 11:20:07 +0330

    add Tab Navigator

--- App.tsx
+++ App.tsx
@@ -12,39 +12,60 @@ import AddNewCard from "./src/pages/addNewCard/addNewCard";
 import CardsItems from "./src/pages/CardsItems/cardsItems";
 import {StyleSheet} from "react-native";
 
-const App = () => {
-    const Stack = createNativeStackNavigator();
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-    return (
-        <Provider store={store}>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Home" screenOptions={{
-                    headerTitleAlign: "center",
-                    statusBarHidden: true,
-                    headerStyle: styles.headerStyle,
-                    headerTintColor:'#ffffff',
-                   headerTitleStyle:{
-                       fontFamily: "Vazir-Medium-FD-WOL",
-                   }
-                }}>
-                    <Stack.Screen name="Home" options={{title:'لایتنر'}} component={HomeScreen}/>
-                    <Stack.Screen name="ShowCard" component={ShowCard}/>
-                    <Stack.Screen name="EditCard" options={{title:'ویرایش کارت'}} component={EditCard}/>
-                    <Stack.Screen name="CardsItems" component={CardsItems}/>
-                    <Stack.Screen name="AddNewCard" options={{title:'افزودن کارت جدید'}} component={AddNewCard}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-        </Provider>
-    );
+function LightnerTab() {
+  const Stack = createNativeStackNavigator();
+  return (
+      <Stack.Navigator initialRouteName="Home" screenOptions={{
+        headerTitleAlign: "center",
+        statusBarHidden: true,
+        headerStyle: styles.headerStyle,
+        headerTintColor:'#ffffff',
+        headerTitleStyle:{
+          fontFamily: "Vazir-Medium-FD-WOL",
+        }
+      }}>
+        <Stack.Screen name="Home" options={{title:'لایتنر'}} component={HomeScreen}/>
+        <Stack.Screen name="ShowCard" component={ShowCard}/>
+        <Stack.Screen name="EditCard" options={{title:'ویرایش کارت'}} component={EditCard}/>
+        <Stack.Screen name="CardsItems" component={CardsItems}/>
+        <Stack.Screen name="AddNewCard" options={{title:'افزودن کارت جدید'}} component={AddNewCard}/>
+      </Stack.Navigator>
+  );
+}
+
+function SettingsTab() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const App = () => {
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{headerShown:false}}>
+          <Tab.Screen name="LightnerTab" component={LightnerTab} />
+          <Tab.Screen name="Settings" component={SettingsTab} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-    headerStyle: {
-        backgroundColor: '#5a5ba2',
-        color:'#b2b3d8',
-        borderRadius:80,
-        fontFamily: "Vazir-Medium-FD-WOL",
-    },
+  headerStyle: {
+    backgroundColor: '#5a5ba2',
+    color:'#b2b3d8',
+    borderRadius:80,
+    fontFamily: "Vazir-Medium-FD-WOL",
+  },
 })
