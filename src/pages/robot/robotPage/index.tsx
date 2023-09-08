e2e4a6a68c97b3c13ff089328701e0229e870db3
Amin Nateghi <aminnateghi10@gmail.com>
2023-09-08 14:57:28 +0330

    add err response chat GPT

--- src/pages/robot/robotPage/index.tsx
+++ src/pages/robot/robotPage/index.tsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import {StyleSheet} from 'react-native';
+import axios from "axios";
+import { StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
-import {GiftedChat, InputToolbar} from 'react-native-gifted-chat';
+import { GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { useTheme } from "../../../context/themeContext";
@@ -11,14 +11,14 @@ const ChatScreen = () => {
 
   const styles = StyleSheet.create({
     messageContainer: {
-      paddingBottom: 16,
+      paddingBottom: 0
     },
     input: {
       borderColor: currentTheme.border,
-      justifyContent:'center',
+      justifyContent: "center",
       borderWidth: 0,
-      borderRadius: 4,
-    },
+      borderRadius: 4
+    }
   });
 
   const [messages, setMessages] = useState([]);
@@ -31,12 +31,12 @@ const ChatScreen = () => {
   // Load messages from AsyncStorage
   const loadMessages = async () => {
     try {
-      const storedMessages = await AsyncStorage.getItem('chatMessages');
+      const storedMessages = await AsyncStorage.getItem("chatMessages");
       if (storedMessages) {
         setMessages(JSON.parse(storedMessages));
       }
     } catch (error) {
-      console.error('Error loading messages from AsyncStorage:', error);
+      console.error("Error loading messages from AsyncStorage:", error);
     }
   };
 
@@ -44,29 +44,28 @@ const ChatScreen = () => {
     setIsTyping(true);
     try {
       const response = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
+        "https://api.openai.com/v1/chat/completions",
         {
           messages: [
             {
-              role: 'user',
-              content: message,
-            },
+              role: "user",
+              content: message
+            }
           ],
-          model: 'gpt-3.5-turbo',
+          model: "gpt-3.5-turbo"
         },
         {
           headers: {
-            Authorization: `Bearer ${'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'}`,
-            'Content-Type': 'application/json',
-          },
-        },
+            Authorization: `Bearer ${"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"}`,
+            "Content-Type": "application/json"
+          }
+        }
       );
 
       return response.data.choices[0].message.content;
     } catch (err) {
-      console.log(err, 'api call error');
-    }
-    finally {
+      console.log(err, "api call error");
+    } finally {
       setIsTyping(false);
     }
   };
@@ -82,35 +81,38 @@ const ChatScreen = () => {
     const chatMessage = [
       {
         _id: Math.random().toString(36).substring(7),
-        text: response,
+        text: response ?? "مشکلی وجود دارد! \nلطفا اینترنت خود را چک کنید. \nدر غیر این صورت با پشتیبان تماس بگیرید. (تنظیمات)",
         createdAt: new Date(),
         user: {
           _id: 2,
-          name: 'GPT-3.5-turbo',
-          avatar: require('../../../../assets/img/avatar/botAvatar.png'),
-        },
-      },
+          name: "GPT-3.5-turbo",
+          avatar: require("../../../../assets/img/avatar/botAvatar.png")
+        }
+      }
     ];
+
 
     // Append the chat message to the state
     setMessages((prevMessages) => GiftedChat.append(prevMessages, chatMessage));
 
     // Save the updated messages to AsyncStorage
     try {
-      await AsyncStorage.setItem('chatMessages', JSON.stringify(messages));
+      await AsyncStorage.setItem("chatMessages", JSON.stringify(messages));
     } catch (error) {
-      console.error('Error saving messages to AsyncStorage:', error);
+      console.error("Error saving messages to AsyncStorage:", error);
     }
+
   };
 
   const user = {
     _id: 1,
-    name: 'Developer',
-    avatar: require('../../../../assets/img/avatar/userAvatar.png'),
+    name: "Developer",
+    avatar: require("../../../../assets/img/avatar/userAvatar.png")
   };
 
   const renderInputToolbar = props => {
-    return <InputToolbar {...props} containerStyle={styles.input} primaryStyle={{backgroundColor:currentTheme.card}} />;
+    return <InputToolbar {...props} containerStyle={styles.input}
+                         primaryStyle={{ backgroundColor: currentTheme.card }} />;
   };
 
   return (
@@ -118,7 +120,7 @@ const ChatScreen = () => {
       messages={messages}
       onSend={onSend}
       user={user}
-      placeholder={'پیام'}
+      placeholder="پیام"
       showUserAvatar={true}
       showAvatarForEveryMessage={true}
       isTyping={isTyping}
