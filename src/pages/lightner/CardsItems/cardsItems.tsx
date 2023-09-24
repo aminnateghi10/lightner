c8859e225c4b0802fdbc1bef88ac3f70b670e67d
Amin Nateghi <aminnateghi10@gmail.com>
2023-09-24 23:15:48 +0330

    add Controller for cardsItems

--- src/pages/lightner/CardsItems/cardsItems.tsx
+++ src/pages/lightner/CardsItems/cardsItems.tsx
@@ -1,63 +1,67 @@
-import {Colors} from "../../../constants/colors";
+import { useState } from "react";
 import ArrowLeft from "react-native-vector-icons/AntDesign";
-import { FlatList, ScrollView, StyleSheet, View } from "react-native";
+import { FlatList, SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
 
 import Card from "./components/card";
-import {useAppSelector} from "../../../store";
 import MyText from "../../../shared/myText";
-import { useState } from "react";
+import { useAppSelector } from "../../../store";
+import { Colors } from "../../../constants/colors";
+import Controller from "./components/controller";
 
-const CardsItems = ({route, navigation}: any) => {
-    const {listName} = route.params;
-    const [dropDown, setDropDown] = useState(null);
+const CardsItems = ({ route, navigation }: any) => {
+  const Styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+    headerContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      backgroundColor: Colors.card,
+      height: 55,
+      padding: 12,
+      borderColor: "red"
+    },
+    headerTitle: {
+      fontSize: 18
+    }
+  });
 
-    const cards = useAppSelector(state => state.cards);
-    let list = cards.list.find(item => item.name == listName);
-    navigation.setOptions({
-        header: () => (
-            <View style={Styles.headerContainer}>
-                <ArrowLeft style={{textAlign:'left'}} name="arrowleft" size={27} onPress={()=>navigation.goBack()}/>
-                <View style={{flexDirection:"row-reverse",alignItems:'center'}}>
-                    <MyText style={Styles.headerTitle}>دسته: {listName}</MyText>
-                    <MyText style={{
-                        marginHorizontal: 10,
-                        padding: 4,
-                        backgroundColor: Colors.button,
-                        borderRadius: 5,
-                    }}>{list?.cards.length}</MyText>
-                </View>
-            </View>
-        ),
-    })
+  const { listName } = route.params;
+  const [dropDown, setDropDown] = useState(null);
 
-    return (
-        <ScrollView
-          style={Styles.container}
-        contentContainerStyle={{paddingBottom:80}}>
-            {
-                list?.cards.map((item,index) =>(
-                  <Card dropDown={dropDown} setDropDown={setDropDown} index={index} navigation={navigation} data={item} listName={list?.name ?? ''}/>
-                ))
-            }
-        </ScrollView>
-    );
+  const cards = useAppSelector(state => state.cards);
+  let list = cards.list.find(item => item.name == listName);
+  navigation.setOptions({
+    header: () => (
+      <View style={Styles.headerContainer}>
+        <ArrowLeft style={{ textAlign: "left" }} name="arrowleft" size={27} onPress={() => navigation.goBack()} />
+        <View style={{ flexDirection: "row-reverse", alignItems: "center" }}>
+          <MyText style={Styles.headerTitle}>دسته: {listName}</MyText>
+          <MyText style={{
+            marginHorizontal: 10,
+            padding: 4,
+            backgroundColor: Colors.button,
+            borderRadius: 5
+          }}>{list?.cards.length}</MyText>
+        </View>
+      </View>
+    )
+  });
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView style={Styles.container} contentContainerStyle={{ paddingBottom: 80 }}>
+        {
+          list?.cards.map((item, index) => (
+            <Card dropDown={dropDown} setDropDown={setDropDown} index={index} navigation={navigation} data={item}
+                  listName={list?.name ?? ""} />
+          ))
+        }
+      </ScrollView>
+      <Controller/>
+    </SafeAreaView>
+  );
 };
 
 export default CardsItems;
 
-const Styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    headerContainer: {
-        flexDirection:'row',
-        justifyContent: 'space-between',
-        backgroundColor: Colors.card,
-        height: 55,
-        padding:12,
-        borderColor: 'red',
-    },
-    headerTitle: {
-        fontSize: 18
-    },
-});
---
+++ src/pages/lightner/CardsItems/components/controller.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View } from "react-native";
+
+const Controller = () => {
+  const Styles = StyleSheet.create({
+    container: {
+      backgroundColor: "red",
+      width: "100%",
+      height: 50
+    }
+  });
+
+  return (
+    <View style={Styles.container}>
+        
+    </View>
+  );
+};
+
+export default Controller;
