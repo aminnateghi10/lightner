081a5ad9864cc68fad1533eeda191f32405c6dbd
Amin Nateghi <aminnateghi10@gmail.com>
2024-02-02 23:00:57 +0330

    allowRTL(false) in app

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry,I18nManager} from 'react-native';
+
+I18nManager.allowRTL(false) ;
 
 import App from './App';
 import {name as appName} from './app.json';
